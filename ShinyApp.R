2f3311233a51ce21917831f3a45ae5fa31e024b9
Kolton Begley <99286975+kolhbegl@users.noreply.github.com>
2022-03-09 16:28:51 -0500

    Update ShinyApp.R
    Got the Volume Graphic to work

--- ShinyApp.R
+++ ShinyApp.R
@@ -14,6 +14,7 @@ clean_names <- function(stocks) {
   vapply(split_names, function(x) x[2], character(1))
 }
 
+
 ui <-
   dashboardPage( skin = "green",
                  dashboardHeader(title = "What if I invested?", titleWidth = 400),
@@ -23,7 +24,7 @@ ui <-
                               icon = icon("dashboard")),
                      menuItem("Stock Prices Graphic", tabName = "graph", 
                               icon = icon("table")),
-                     menuItem("Feature 3", tabName = "feature3", 
+                     menuItem("Volume Graphic", tabName = "feature3", 
                               icon = icon("th")),
                      menuItem("Feature 4", tabName = "feature4", 
                               icon = icon("th"))
@@ -53,9 +54,9 @@ ui <-
                      
                      # Second tab content
                      tabItem(tabName = "graph",
-                             h2("Graph My Stock"),
+                             h1("Graph My Stock Prices"),
                              basicPage(
-                               h1("Stock Prices"),
+                               h2("Stock Prices"),
                                textInput("stocks", "Input the ticker symbol of your choice", 
                                          "FLWS"),   
                                dateRangeInput("date", "Choose the desired date range for your stock of 
@@ -72,9 +73,18 @@ ui <-
                      
                      # Third tab content
                      tabItem(tabName = "feature3",
-                             h2("Feature 3")
-                             
-                             
+                             h1("Plot the Volume of My Stock"), 
+                             basicPage(
+                               textInput("stocks1", "Input the ticker symbol of your choice", 
+                                         "FLWS"),   
+                               dateRangeInput("date1", "Choose the desired date range for your stock of 
+                       interest (Jan 2007 - Jan 2022)",
+                                              start = "2013-01-01", 
+                                              end = "2022-01-01",min = "2007-01-01", 
+                                              max = "2022-02-01",
+                                              format = "yyyy-mm-dd" ),
+                               plotlyOutput("volumeplot")
+                             )
                      ),
                      
                      # Fourth tab content
@@ -104,5 +114,14 @@ server <- function(input, output, session) {
     chartSeries(data, theme = chartTheme("white"),
                 type = "line", log.scale = input$log, TA = NULL)
   })
+  output$volumeplot <- renderPlotly({
+    data1<- getSymbols(input$stocks1, from = input$date1[1],
+                       to = input$date1[2], src = "yahoo",
+                       auto.assign = FALSE)
+    names(data1) <- clean_names(data1)
+    autoplot(data1$Volume) %>% ggplotly() 
+    
+  })
 }
 shinyApp(ui, server)
+
